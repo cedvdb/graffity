41fb3615ab165db4cc4375dcc59e7656c2fe0639
cedvdb <cedvandenbosch@gmail.com>
2020-06-04 21:22:16 +0200

    some styling

--- src/app/components/user-picture/user-picture.component.ts
+++ src/app/components/user-picture/user-picture.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-user-picture',
   templateUrl: './user-picture.component.html',
   styleUrls: ['./user-picture.component.scss'],
   host: {
-    '[class.m]': 'size === "m"'
+    '[class.m]': 'size === "m"',
+    '[class.l]': 'size === "l"'
   }
 })
 export class UserPictureComponent implements OnInit {
 
   @Input() url;
-  @Input() size = 'm';
+  @Input() size = 1;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 
   ngOnInit(): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'height', `${this.size}rem`);
+    this.renderer.setStyle(this.elRef.nativeElement, 'width', `${this.size}rem`);
   }
 
 }
